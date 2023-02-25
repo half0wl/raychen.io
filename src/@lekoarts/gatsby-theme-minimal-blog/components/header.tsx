4b33612a2db31bdf4ba11a81859cac3bbd246436
Ray Chen <ray@raychen.io>
2023-02-26 04:55:31 +0800

    Header: Remove ColorModeToggle

---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/header.tsx
@@ -0,0 +1,36 @@
+/** @jsx jsx */
+import { jsx, Flex } from "theme-ui"
+import useMinimalBlogConfig from "@lekoarts/gatsby-theme-minimal-blog/src/hooks/use-minimal-blog-config"
+import Navigation from "@lekoarts/gatsby-theme-minimal-blog/src/components/navigation"
+import HeaderTitle from "@lekoarts/gatsby-theme-minimal-blog/src/components/header-title"
+import HeaderExternalLinks from "@lekoarts/gatsby-theme-minimal-blog/src/components/header-external-links"
+
+const Header = () => {
+  const { navigation: nav } = useMinimalBlogConfig()
+
+  return (
+    <header sx={{ mb: [3, 4] }}>
+      <Flex sx={{ alignItems: `center`, justifyContent: `space-between` }}>
+        <HeaderTitle />
+      </Flex>
+      <div
+        sx={{
+          boxSizing: `border-box`,
+          display: `flex`,
+          variant: `dividers.bottom`,
+          alignItems: `center`,
+          justifyContent: `space-between`,
+          mt: 3,
+          color: `secondary`,
+          a: { color: `secondary`, ":hover": { color: `heading` } },
+          flexFlow: `wrap`,
+        }}
+      >
+        <Navigation nav={nav} />
+        <HeaderExternalLinks />
+      </div>
+    </header>
+  )
+}
+
+export default Header
