14b2a2664bfb76753ff78545cacff7fb92b8fb7b
Ray Chen <ray@raychen.io>
2023-02-26 08:12:31 +0800

    Tweak 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,11 @@
-import * as React from "react"
-import type { PageProps } from "gatsby"
-import Layout from "@lekoarts/gatsby-theme-minimal-blog/src/components/layout"
+import * as React from 'react'
+import type { PageProps } from 'gatsby'
+import Layout from '@lekoarts/gatsby-theme-minimal-blog/src/components/layout'
 
 const NotFound = (_props: PageProps) => (
   <Layout>
     <h1>404 - Page Not Found</h1>
-    <p>Unfortunately we couldn't find what you were looking for :(</p>
+    <p>This page does not exist. How did you get here?!</p>
   </Layout>
 )
 
