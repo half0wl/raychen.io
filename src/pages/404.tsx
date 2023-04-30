a28497829a33c9b092a88067d2a037ea56420eca
Ray Chen <ray@raychen.io>
2023-05-01 02:46:23 +0800

    404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,14 @@
+import Link from 'next/link'
+
 const NotFound = () => {
-  return <>Not found</>
+  return (
+    <div className="my-20">
+      <h1 className="text-2xl font-bold">404 Not Found</h1>
+      <p className="text-lg">
+        Click <Link href="/">here</Link> to go back.
+      </p>
+    </div>
+  )
 }
 
 export default NotFound
