be66caa32d67996d0d0d730de5bf325cc8b906e7
Ray Chen <ray@raychen.io>
2023-10-23 01:10:39 +0800

    Revamp ext article link

--- src/components/article-list.tsx
+++ src/components/article-list.tsx
@@ -1,37 +1,26 @@
-import Link from 'next/link'
 import { ParsedArticle } from '@/lib/build'
-import classNames from 'classnames'
+import Link from 'next/link'
 
 const ArticleList: React.FC<{ articles: ParsedArticle[] }> = ({ articles }) => {
   return (
     <ul>
       {articles.map((a) => (
-        <li
-          className={classNames('mb-2', a.publication && 'relative')}
-          key={a.slug}
-        >
+        <li className="mb-2" key={a.slug}>
+          <span className="mr-2 font-mono text-slate-500">{a.publishedAt}</span>
+          <Link className="text-lg" href={a.slug}>
+            {a.title}
+          </Link>
           {a.publication && (
-            <span className="absolute left-[-2%] opacity-50"></span>
-          )}
-          <>
-            <span className="mr-2 font-mono text-slate-500">
-              {a.publishedAt}
+            <span>
+              {' '}
+              &mdash;{' '}
+              {a.publicationUrl ? (
+                <Link href={a.publicationUrl}>⧉ {a.publication}</Link>
+              ) : (
+                a.publication
+              )}
             </span>
-            <Link className="text-lg" href={a.slug}>
-              {a.title}
-            </Link>
-            {a.publication && (
-              <span>
-                {' '}
-                &mdash;{' '}
-                {a.publicationUrl ? (
-                  <Link href={a.publicationUrl}>⧉ {a.publication}</Link>
-                ) : (
-                  a.publication
-                )}
-              </span>
-            )}
-          </>
+          )}
         </li>
       ))}
     </ul>
